a8cc2e20232ce34aa9eef10a934fe191c5036627
milan338 <56327606+milan338@users.noreply.github.com>
2021-02-03 16:32:58 +1030

    Improved python version enforcement

--- setup.py
+++ setup.py
@@ -14,16 +14,37 @@
 # You should have received a copy of the GNU General Public License
 # along with FreeRGB.  If not, see <https://www.gnu.org/licenses/>.
 
+import sys
+import setuptools
 from pathlib import Path
 
 path = Path(__file__).parent.resolve()
+
+# Get version data
 __version__ = (path / '__version__.py').read_text(encoding='utf-8')
-
-version = None
+python_version = sys.version_info[:2]
 python_requires = None
-
+prog_version = None
 for line in __version__.split('\n'):
     if '__version__' in line:
-        version = line.strip("__version__ ='")
+        prog_version = line.strip("__version__ ='")
     elif 'python_requires' in line:
-        python_requires = line.strip("python_requires ='")
+        requires_as_str = line.strip(
+            "python_requires ='").strip('()').split(', ')
+        python_requires = (int(requires_as_str[0]), int(requires_as_str[1]))
+
+# Enforce version
+if python_version < python_requires:
+    sys.stderr.write(f"""
+    ==========================
+    Unsupported Python version
+    ==========================
+
+    Your system is running Python {python_version[0]}.{python_version[1]}
+    which is lower than the required Python {python_requires[0]}.{python_requires[1]}.
+
+    Please consider updating your installation.
+    """)
+    sys.exit(1)
+
+setuptools.setup()
